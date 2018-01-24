3cc336f2b9294f9144a7fd914dcff4daa6266a7a
Imrann0770180 <imrancool023@gmail.com>
2018-01-24 12:42:18 +0000

    Wed, Jan 24, 2018 12:42:17 PM

---
+++ Lab3/sensor/sensor.ino
@@ -0,0 +1,33 @@
+#define ECHOPIN 3
+#define TRIGPIN 2
+int led = 9;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(ECHOPIN, INPUT);
+  pinMode(TRIGPIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  digitalWrite(TRIGPIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIGPIN,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGPIN,LOW); 
+  float distance = pulseIn(ECHOPIN, HIGH); 
+  distance = distance/29 / 2;
+  Serial.print(distance);
+  Serial.println("cm");
+  delay(200);
+  if (distance < 10) 
+  {
+  for (int j=1; j<=5; j++) {   
+    digitalWrite(led,HIGH); 
+    delay(200);             
+    digitalWrite(led,LOW); 
+    delay(200);            
+}
+   }
+}
+      
