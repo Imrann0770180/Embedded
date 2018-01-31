2da01d0f1847be392ccc623e3a4fde1783c078c3
Imrann0770180 <imrancool023@gmail.com>
2018-01-31 12:43:33 +0000

    Wed, Jan 31, 2018 12:43:32 PM

--- bluetooth1/bluetooth1.ino
+++ bluetooth1/bluetooth1.ino
@@ -1,45 +1,24 @@
-/*
-  Bluetooth communication
+#define ECHOPIN 3
+#define TRIGPIN 2
 
-  By putting Serial this will print the messages in the serial monitor
-  By putting Serial1 this will print the messages in putty window
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(ECHOPIN, INPUT);
+  pinMode(TRIGPIN, OUTPUT);
 
-  Was unsure what you wanted for task 14 of the lab but I understand how to display the message in
-  the arduino program and external program like putty.
- */
-int led1 = 11;                          // Use digital pin 11 for the led
-char inVal;                             // Use this to read a character from the serial port
-
-/****************************************************************************************************************************/
-
-void setup() 
-{
-  pinMode(led1, OUTPUT);                // Initialise digital pin for the LED
-  //Serial.begin(115200);                // Set the Baudrate to 115200 on putty as well (note which serial port) 
-  Serial1.begin(115200);
 }
 
-/***************************************************************************************************************************/
-
-void loop() 
-{
-  if (Serial1.available()>0)            // Will only read the serial when something is entered (have to enter the x twice to stop)
-    {
-    inVal=Serial1.read();               // Reads data from the serial port
-    
-    if(inVal == '1')                    // If you enter 1 the  first LED will turn on then off
-    {                                   
-      digitalWrite(led1, HIGH);         // Turns the LED 1 on
-      Serial1.println("You entered 1"); // Prints out this message
-      delay(200);                      // Wait for 2 seconds
-      //digitalWrite(led1, LOW);          // Turns the LED 1 off
-    }
-
-   else                                 // If there is no input from the serial line, turn the LED's off and displays a message
-  {
-      digitalWrite(led1, LOW);
-      Serial1.println("Waiting for command.......");   
-      delay(20);
-  }
-    }
+void loop() {
+  // put your main code here, to run repeatedly: 
+  digitalWrite(TRIGPIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIGPIN,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGPIN,LOW); 
+ float distance = pulseIn(ECHOPIN, HIGH); // return 0 if no high pulse in 1sec
+ Serial.print(distance);
+ Serial.println("cm");
+ delay(200);
+  
 }
